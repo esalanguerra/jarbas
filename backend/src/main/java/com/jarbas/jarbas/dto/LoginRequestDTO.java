b73eda500dba71ecb9b50910188bd4092b265c9d
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-04 11:13:05 -0300

    Adicionando DTOs

---
+++ backend/src/main/java/com/jarbas/jarbas/dto/LoginRequestDTO.java
@@ -0,0 +1,3 @@
+package com.jarbas.jarbas.dto;
+
+public record LoginRequestDTO (String email, String password){}
---
+++ backend/src/main/java/com/jarbas/jarbas/dto/ResponseDTO.java
@@ -0,0 +1,3 @@
+package com.jarbas.jarbas.dto;
+
+public record ResponseDTO (String name, String token) { }
