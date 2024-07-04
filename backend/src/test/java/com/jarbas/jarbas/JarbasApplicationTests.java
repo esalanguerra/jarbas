5adec081e7e932cea83073167c84ed489f44fc91
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-04 10:44:10 -0300

    start spring boot

---
+++ backend/src/test/java/com/jarbas/jarbas/JarbasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarbas.jarbas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarbasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
