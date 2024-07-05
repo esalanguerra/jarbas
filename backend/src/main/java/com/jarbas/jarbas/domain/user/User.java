832080b5b43ac4d22387f0b774ebf75d9a78d211
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-05 08:50:39 -0300

    Adicionando lombok

--- backend/src/main/java/com/jarbas/jarbas/domain/user/User.java
+++ backend/src/main/java/com/jarbas/jarbas/domain/user/User.java
@@ -6,8 +6,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
