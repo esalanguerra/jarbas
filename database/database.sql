e16f2f3bf784709b64655d3a5f82856c8bdf33b3
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-05 18:46:09 -0300

    criando relacionamento

--- database/database.sql
+++ database/database.sql
@@ -14,10 +14,19 @@ CREATE TABLE `form_associations` (
 
 CREATE TABLE `form_domains` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `form_fields` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `form_domain_fields` (
+  `form_domain_id` int(11) NOT NULL,
+  `form_field_id` int(11) NOT NULL,
+  PRIMARY KEY (`form_domain_id`, `form_field_id`),
+  FOREIGN KEY (`form_domain_id`) REFERENCES `form_domains`(`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`form_field_id`) REFERENCES `form_fields`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
