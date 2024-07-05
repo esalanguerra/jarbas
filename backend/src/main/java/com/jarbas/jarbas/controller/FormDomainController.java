c58f52bf551b51f8cf2c6459da69215cd9f29795
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-05 18:45:41 -0300

    Adicionando mapeamento de endpoints

--- backend/src/main/java/com/jarbas/jarbas/controller/FormDomainController.java
+++ backend/src/main/java/com/jarbas/jarbas/controller/FormDomainController.java
@@ -2,12 +2,27 @@ package com.jarbas.jarbas.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jarbas.jarbas.dto.CreateFormDomainDTO;
+import com.jarbas.jarbas.repositories.FormDomainRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/form-domains")
+@RequiredArgsConstructor
 public class FormDomainController {
+  private final FormDomainRepository repository;
+
+  @PostMapping("/create")
+  public ResponseEntity createFormDomain(@RequestBody CreateFormDomainDTO createFormDomainDto) {
+    return ResponseEntity.ok("sucesso!");
+  }
+
   @GetMapping
   public ResponseEntity<String> getFormDomains() {
     return ResponseEntity.ok("sucesso!");
--- backend/src/main/java/com/jarbas/jarbas/controller/FormFieldController.java
+++ backend/src/main/java/com/jarbas/jarbas/controller/FormFieldController.java
@@ -2,14 +2,34 @@ package com.jarbas.jarbas.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jarbas.jarbas.repositories.FormFieldRepository;
+import com.jarbas.jarbas.domain.FormField.FormField;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/form-fields")
+@RequiredArgsConstructor
 public class FormFieldController {
-  @GetMapping
+
+  private final FormFieldRepository repository;
+
+  @PostMapping("/create")
+  public ResponseEntity<String> createFormField() {
+    return ResponseEntity.ok("sucesso!");
+  }
+
+  @GetMapping("/list")
   public ResponseEntity<String> getFormFields() {
+    return ResponseEntity.ok("sucesso!");
+  }
+
+  @GetMapping("/list/{id}")
+  public ResponseEntity<String> getFormFieldById() {
     return ResponseEntity.ok("sucesso!");
   }
 }
