22b00e481c42af1beee526ee44ba0403c765fa2d
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-06 19:49:11 -0300

    Autenticação com firebase

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,37 @@
+"use client";
+import {firebaseConfig} from "@jarbas/config/FirebaseConfig";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { useForm } from "react-hook-form";
+
 export default function Login() {
+  const auth = getAuth(firebaseConfig);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  async function onSubmit(data: any) {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      console.log(userCredential);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <></>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...register("email")} />
+      <input type="password" {...register("password")} />
+      <button type="submit">Login</button>
+    </form>
   );
 }
