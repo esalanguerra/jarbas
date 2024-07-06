ac981a0a56e44e8066b9a3a5e13208903b8f91fe
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-06 12:20:02 -0300

    criando interfaces

---
+++ src/interfaces/IFormInterface.d.ts
@@ -0,0 +1,8 @@
+import { IFormInputInterface } from "./IFormInputInterface";
+
+export interface IFormInterface {
+  sub: string;
+  title: string;
+  description: string;
+  inputs: IFormInputInterface[];
+}
