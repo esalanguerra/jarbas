1830621c30bb38c8e9c4eb7e5e78a539dcea4666
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2024-07-07 13:23:48 -0300

    tail

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,6 +2,8 @@
 import { AppFirebase } from "@jarbas/libs/firebase/AppFirebase";
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider } from "firebase/auth";
 import { useForm } from "react-hook-form";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faApple } from '@fortawesome/free-brands-svg-icons';
 
 export default function Login() {
   const auth = getAuth(AppFirebase);
@@ -9,7 +11,6 @@ export default function Login() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
 
@@ -21,8 +22,7 @@ export default function Login() {
         data.password
       );
       console.log(userCredential);
-    }
-    catch (error) {
+    } catch (error) {
       console.error(error);
     }
   }
@@ -30,13 +30,63 @@ export default function Login() {
   async function onSubmitGoogle() {}
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" {...register("email")} />
-      <input type="password" {...register("password")} />
-      <button type="submit">Login</button>
-      <button type="button" onClick={onSubmitGoogle}>
-        Login with Google
-      </button>
-    </form>
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('/images/background_login.png')" }}>
+      <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-7xl p-8 h-4/5">
+        <div className="text-white md:mr-8 mb-8 md:mb-0 max-w-md">
+          <h1 className="text-7xl font-bold mb-40" style={{color:'#EAB308'}}>Dr. Jarbas</h1>
+          <p className="text-lg mt-20" style={{color:'#A3A3A3'}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a bibendum lacus, in semper lorem. Proin arcu leo, maximus eu sagittis eget, egestas nec leo.
+          </p>
+        </div>
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full py-8">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 py-8 ">
+            <div className="py-2">
+              <label className="block text-sm font-medium text-gray-700 ">Email</label>
+              <input
+                type="text"
+                {...register("email", { required: true })}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"
+                placeholder="seu@email.com"
+              />
+              {errors.email && <span className="text-red-500 text-sm">Email é obrigatório</span>}
+            </div>
+            <div className="py-2">
+              <label className="block text-sm font-medium text-gray-700">Senha</label>
+              <input
+                type="password"
+                {...register("password", { required: true })}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"
+                placeholder="********"
+              />
+              {errors.password && <span className="text-red-500 text-sm">Senha é obrigatória</span>}
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-yellow-500 text-white py-2 rounded-md shadow-sm hover:bg-yellow-600"
+            >
+              Entrar
+            </button>
+          </form> 
+          <div className="mt-6 py-8">
+            <button
+              type="button"
+              onClick={onSubmitGoogle}
+              className="w-full bg-white text-black py-2 rounded-md shadow-sm border flex items-center justify-center space-x-2 hover:bg-gray-100"
+            >
+              <FontAwesomeIcon icon={faGoogle} className="w-5 h-5" />
+              <span>Sign in with Google</span>
+            </button>
+            <div className="text-center text-gray-500 mt-2">OR</div>
+            <button
+              type="button"
+              className="w-full bg-black text-white py-2 rounded-md shadow-sm flex items-center justify-center space-x-2 hover:bg-gray-800"
+            >
+              <FontAwesomeIcon icon={faApple} className="w-5 h-5" />
+              <span>Sign in with Apple</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import './styles/global.css';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html className="min-h-full" lang="pt-br">
       <body className={inter.className}>{children}</body>
     </html>
   );
