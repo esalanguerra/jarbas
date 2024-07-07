7594150fe775e286cf129a463961f7c2da55df3a
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-06 21:07:00 -0300

    tipando as constantes do firebase

--- src/config/FirebaseConfig.ts
+++
@@ -1,7 +0,0 @@
-import { firebaseConstants } from "@jarbas/constants/FirebaseConstants";
-import {initializeApp, getApps} from "firebase/app";
-
-const firebaseConfig = getApps()
-  .length === 0 ? initializeApp(firebaseConstants) : getApps()[0];
-
-export { firebaseConfig };
---
+++ src/interfaces/IFirebaseConstantsInterface.d.ts
@@ -0,0 +1,9 @@
+export interface IFirebaseConstantsInterface {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+}
