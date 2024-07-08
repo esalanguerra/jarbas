ed431ed7c4e59a33c5f34ae31cb7defd7aa44751
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-08 05:14:43 -0300

    listando os formulários em tabelas

---
+++ src/app/list-forms/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { JarbasFormDomainsCollections } from '@jarbas/libs/firebase';
+import { useEffect, useState } from 'react';
+import { doc, getDocs } from 'firebase/firestore';
+import { IFormInterface } from '@jarbas/interfaces/IFormInterface';
+
+export default function ListForms() {
+  const [formDomains, setFormDomains] = useState<IFormInterface[]>([]);
+
+  useEffect(() => {
+    async function getFormDomains() {
+      const formDomains = await getDocs(JarbasFormDomainsCollections);
+
+      const formDomainsData = formDomains.docs.map(doc => {
+        return {
+          ...doc.data(),
+          sub: doc.id
+        };
+      });
+
+      setFormDomains(formDomainsData);
+    }
+
+    getFormDomains();
+  }, []);
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Descrição</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            formDomains.map(formDomain => (
+              <tr key={formDomain.sub}>
+                <td>{formDomain.title}</td>
+                <td>{formDomain.description}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </>
+  );
+}
