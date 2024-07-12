b5990a93dfabc3bc628aeecdddc0c7a5024b4dca
Alan Lima da Silva Guerra <alanlimadasilvaguerra@outlook.com.br>
2024-07-12 06:18:27 -0300

    redirecionamentos

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Redirects urls
+  async redirects() {
+    return [
+      {
+        source: "/app",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/dashboard",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/auth/login",
+        destination: "/login",
+        permanent: true,
+      },
+      {
+        source: "/entrar",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
